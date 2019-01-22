ff6ca85b39792c577f047cc7a7b2dd46502d384d
Ipul Saepulloh <ipulsaepulloh9@gmail.com>
2019-01-22 12:03:48 +0700

    create customer database

---
+++ index.js
@@ -0,0 +1,50 @@
+const PORT = 3000
+
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser")
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+
+const mongoose = require("mongoose");
+const MONGODB_URI = "mongodb://localhost:27017/impactbyte"
+mongoose.connect(
+    MONGODB_URI,
+    {
+        useNewUrlParser: true
+    }
+);
+
+const Schema = mongoose.Schema;
+const Customers = mongoose.model("customers", new Schema({
+    first_name: String,
+    last_name: String,
+    age: Number,
+    address: {
+        street: String,
+        city: String,
+        state: String,
+    }
+}, { timestamps: true }))
+
+app.post("/customers",(req,res) =>{
+    Customers.create(req.body, (err, customers) => {
+        if (err) return res.send("gagal cuy")
+
+console.lo
+        res.send("SUKSES MANTAP");
+    });
+});
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log('App running on PORT ${PORt}')
+})
